1f9a81a7f2ecf4b5aa9f7b56480cc3ca4668ee88
Alvin <alvin.liang@mail.utoronto.ca>
2022-06-30 18:23:53 -0400

    completed stack

--- stack.java
+++ stack.java
@@ -1,18 +1,29 @@
-public class stack<T> {
+import java.util.*;
+public class stack<T> implements Iterable<T> {
     //we're going to implement this using a linked list
     int size;
     Node head = null;
+    //creates an empty stack 
+    public stack(){}
+    //creates a stack witht the first element 
+    public stack(T firstElement){
+        push(firstElement);
+    }
     public T peek() {
         //return the first element of the stack without popping the element from the stack
-        return this.top.val;
+        return this.head.val;
     }
     public void push(T element){
         //place an element on the top of the stack
         Node newNode = new Node(T);
         newNode.next = head;
         this.head = newNode;
+        this.size++;
     }
     public T pop(){
+        if (this.head == null){
+            throw new java.util.EmptyStackException();
+        }
         //removes the top the Node and returns it's value
         Node tempPointer = this.head;
         this.head = head.next;
